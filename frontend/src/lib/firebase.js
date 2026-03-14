365d175e0dc42ea3008f5da0007d7c1f0166edca
surajgit79 <surajg.sci77@hsmonline.edu.np>
2026-03-14 09:32:34 +0545

    Move Firebase config to environment variables

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBcrNmosivvcoI9oFkB07FFzSLiZ0y5HPM",
-    authDomain: "chatty-40890.firebaseapp.com",
-    projectId: "chatty-40890",
-    storageBucket: "chatty-40890.firebasestorage.app",
-    messagingSenderId: "189583791062",
-    appId: "1:189583791062:web:c533fb66f42dd4e17f0a77",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig);
